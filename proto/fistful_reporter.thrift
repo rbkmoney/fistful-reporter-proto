d2ce0d935de5b95393cbad6eee56ced40827384c
WWW_cool <WWW_cool@inbox.ru>
2019-01-17 12:57:07 +0300

    added erlang support

--- proto/fistful_reporter.thrift
+++ proto/fistful_reporter.thrift
@@ -1,16 +1,22 @@
 include "base.thrift"
-include "domain.thrift"
 
 namespace java com.rbkmoney.fistful.reporter
 namespace erlang fistful_reporter
 
 typedef base.Timestamp Timestamp
-typedef base.InvalidRequest InvalidRequest
 typedef i64 ReportID
 typedef base.ID FileID
-typedef domain.PartyID PartyID
-typedef domain.ContractID ContractID
+typedef base.ID PartyID
+typedef base.ID ContractID
 typedef string ReportType
+
+/**
+ * Исключение, сигнализирующее о непригодных с точки зрения бизнес-логики входных данных
+ */
+exception InvalidRequest {
+    /** Список пригодных для восприятия человеком ошибок во входных данных */
+    1: required list<string> errors
+}
 
 /**
 * Ошибка превышения максимального размера блока данных, доступного для отправки клиенту.
